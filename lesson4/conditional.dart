a97244c3eb04972e49be1e107482d4f604c71d58
damunza <danidamunza@gmail.com>
2020-03-19 12:14:34 +0300

    read on conditional statements and discovered indentation blocks are not necessary just there for aesthetics

---
+++ lesson4/conditional.dart
@@ -0,0 +1,20 @@
+// conditional statement
+
+// if else if else
+
+// switch
+
+void main(){
+  var first = 0;
+
+  switch(first){
+    case 3:
+    print('start');
+    break;
+    case 1:
+    print('second');
+    break;
+    default:
+    print('confused');
+  }
+}
