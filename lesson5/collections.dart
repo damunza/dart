6ad03218fc4ad9287c34f8f2865e73a002d4d710
damunza <danidamunza@gmail.com>
2020-03-19 13:04:12 +0300

    lists and basic methods defined the set

---
+++ lesson5/collections.dart
@@ -0,0 +1,23 @@
+// collection
+
+void main(){
+  // list defined by List built in method
+  List first = ['Jack', 'Jill'];
+  print(first[0]); // indexes begin from zero
+  print('\n');
+  print(first.length);  // length of a list
+  print('\n');
+  for(var n in first){
+    print(n);
+  }
+  print('\n');// each element in the list
+
+  /* 
+  -one data type add ; <TYPE> between the variable name and its assignation
+  -constant values ; const added between the equal sign and the list itself
+  -copying lists into lists; [...list being copied]
+  */
+
+  // sets {};
+
+}
