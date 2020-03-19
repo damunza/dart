365a55cf2c6b411425aa9493329d8a2d72ecb389
damunza <danidamunza@gmail.com>
2020-03-19 16:48:46 +0300

    called an object of a class with a constructor

--- lesson7/oop.dart
+++ lesson7/oop.dart
@@ -2,6 +2,7 @@
 
 class Person{
   /// class without a constructor 
+  /// 
   String name;
   int age;
   
@@ -11,10 +12,38 @@ class Person{
   }
 }
 
+class Animal{
+  /// class with a constructor
+  
+String type;
+String name;
+int teeth;
+
+Animal(String type, String name, [int teeth = 42]){
+  this.type = type;
+  this.name = name;
+  this.teeth = teeth;
+}
+
+void showAnimal(){
+    print(name);
+    print(type);
+    print(teeth);
+  }
+}
+
 void main(){
+  // calling a class object from a class without a constructer
 Person person1 = Person();
 person1.name= 'Junior';
 person1.age = 20;
 person1.showPerson();
+print('\n');
+  
+  // calling a class object from a class with a constructer
+
+var animal = Animal('dog', 'bosko');
+animal.showAnimal();
+print('\n');
 }
 
